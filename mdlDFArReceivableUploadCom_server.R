4be520918036bfe8a0f25f782c80ddb0130896b7
takewiki <hulilei@takeiwki.com.cn>
2026-05-20 08:41:06 +0000

    v1.0.0

---
+++ mdlDFArReceivableUploadCom_server.R
@@ -0,0 +1,6 @@
+
+mdlDFArReceivableUploadServer::arReceivableServer(input = input,output = output,session = session,dms_token = dms_token)
+
+
+  
+
---
+++ mdlDFArReceivableUploadCom_ui.R
@@ -0,0 +1,27 @@
+#tabName need to be update
+#tabBox title need to be update
+#tabBox id need to be update
+tabItem(tabName = "mdlDFArReceivableUploadCom",
+                    fluidRow(
+                      column(width = 12,
+                             tabBox(title ="数据中台模块开发DEMO工作台",width = 12,
+                                    id='tabSet_mdlDFArReceivableUploadCom',height = '300px',
+                                    #以上为注册ui组件的内容-----
+                                    #begin----
+                                    #uiComponent seperated by comma (,)
+                                   
+                                    mdlDFArReceivableUploadUI::arReceivableUI()
+                                     
+                                    
+                                    
+                                    #end----
+                                    
+                                    
+                                    
+                                    
+                                    
+                             )
+                      )
+                    )
+)
+ 
